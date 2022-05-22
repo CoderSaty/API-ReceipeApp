7c59bbd364e653bde72d9035bd502c98ddd57d72
Anuranjan Srivastava <codersaty@Anuranjans-MacBook-Air.local>
2022-05-22 16:54:59 +0530

    Added sample for unit test and setup in dockerfile for psycopg2 installation

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator Function
+"""
+
+def add(x,y):
+    """Add x and y"""
+    return x+y
+def sub(x,y):
+    """subtract x and y"""
+    return abs(x-y)
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""
+Sample Tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the Calc Module"""
+    def test_add(self):
+        """Test the adding of two numbers"""
+        res = calc.add(5,6)
+        self.assertEqual(res,11)
+    def test_sub(self):
+        """Test the subtraction of two numbers"""
+        res = calc.sub(5,6)
+        self.assertEqual(res,1)
+    
