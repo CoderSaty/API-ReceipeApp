a1be8c836ca010045b9449ebb480c0bff852de3c
Anuranjan Srivastava <codersaty@Anuranjans-MacBook-Air.local>
2022-05-22 17:04:47 +0530

    fixed linting errors

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,12 @@
 Calculator Function
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y"""
     return x+y
-def sub(x,y):
+
+
+def sub(x, y):
     """subtract x and y"""
     return abs(x-y)
