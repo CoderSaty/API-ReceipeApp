f90464194a136bf547447db9c7f7aed97c375f9a
Anuranjan Srivastava <codersaty@Anuranjans-MacBook-Air.local>
2022-05-24 01:14:09 +0530

    added features like normalize email create super user and email requirred alongwith unit test to the user model

--- app/core/models.py
+++ app/core/models.py
@@ -9,10 +9,19 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("The email field cannot be empty for an user")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(self._db)
 
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
         return user
 
 
